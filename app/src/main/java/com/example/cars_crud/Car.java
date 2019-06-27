636e39ed041767824d1f81723b77481a17cfe758
Rasonakima <Rasonakima@gmail.com>
2019-06-27 13:31:50 +0000

    Added Model car

---
+++ app/src/main/java/com/example/cars_crud/Car.java
@@ -0,0 +1,37 @@
+package com.example.cars_crud;
+
+public class Car {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Car() {
+    }
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
