a352612405868f259ddb0a6797fe7d1cc90c9194
Rasonakima <Rasonakima@gmail.com>
2019-06-27 13:35:46 +0000

    Added car adapter for recycler view

---
+++ app/src/main/java/com/example/cars_crud/CarAdapter.java
@@ -0,0 +1,59 @@
+package com.example.cars_crud;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CarAdapter extends RecyclerView.Adapter<CarAdapter.CarViewHolder> {
+    Context context;
+    private List<Car> cars;
+
+    public class CarViewHolder extends RecyclerView.ViewHolder {
+        public TextView description;
+
+        public CarViewHolder(View view) {
+            super(view);
+            description = (TextView) view.findViewById(R.id.carName);
+        }
+    }
+
+    public void add(int position, Car car) {
+        cars.add(position, car);
+        notifyItemInserted(position);
+    }
+
+    public void remove(Car car) {
+        int position = cars.indexOf(car);
+        cars.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public CarAdapter(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    @NonNull
+    @Override
+    public CarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_car, parent, false);
+        CarViewHolder viewHolder = new CarViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CarViewHolder holder, int position) {
+        holder.description.setText(cars.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return cars.size();
+    }
+}
